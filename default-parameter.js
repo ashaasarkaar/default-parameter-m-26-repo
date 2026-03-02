51a2cb8765b6150907e1a325d028772ebd2a13a0
ashaasarkaar <ashaasarkaar@gmail.com>
2026-03-03 02:35:31 +0600

    / function a sothikvabe paremeter pass na korar jonnno output a NaN asar problem

---
+++ default-parameter.js
@@ -0,0 +1,9 @@
+
+// function a sothikvabe paremeter pass na korar jonnno output a NaN asar problem
+function add(num1, num2) {
+    const addition = num1 + num2;
+    console.log(num1, num2, addition);
+    return addition;
+
+}
+add(10);     // output NaN
